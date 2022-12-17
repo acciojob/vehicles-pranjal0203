2852aa3d049921e9ef0f6adbc3d390eb566b5a28
Pranjal Agarwal <pranjalagarwal6@gmail.com>
2022-12-17 19:11:42 +0530

    new try 2

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -43,7 +43,6 @@ public class Car extends Vehicle {
         super(name);
         this.wheels=wheels;
         this.doors=doors;
-        this.currentGear=1;
         this.gears=gears;
         this.isManual=isManual;
         this.type=type;
